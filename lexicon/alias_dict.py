d15bc99f0436c50171b60459ad4f58a75b403c72
Jeff Forcier <jeff@bitprophet.org>
2012-05-22 14:31:57 -0700

    Another minor refactor

--- lexicon/alias_dict.py
+++ lexicon/alias_dict.py
@@ -18,7 +18,11 @@ class AliasDict(dict):
                 return single(self, target, value)
             # Multi-string targets
             else:
-                return multi(self, target, value)
+                if multi:
+                    return multi(self, target, value)
+                else:
+                    for subkey in target:
+                        single(self, subkey, value)
         else:
             return unaliased(self, key, value)
 
@@ -27,23 +31,16 @@ class AliasDict(dict):
 
     def __setitem__(self, key, value):
         def single(d, target, value): d[target] = value
-
-        def multi(d, target, value):
-            for subkey in target:
-                d[subkey] = value
-
         def unaliased(d, key, value): super(AliasDict, d).__setitem__(key, value)
-
-        return self._handle(key, value, single, multi, unaliased)
+        return self._handle(key, value, single, None, unaliased)
 
     def __getitem__(self, key):
         def single(d, target, value): return d[target]
+        def unaliased(d, key, value): return super(AliasDict, d).__getitem__(key)
 
         def multi(d, target, value):
             msg = "Multi-target aliases have no well-defined value and can't be read."
             raise ValueError(msg)
-
-        def unaliased(d, key, value): return super(AliasDict, d).__getitem__(key)
 
         return self._handle(key, None, single, multi, unaliased)
 
@@ -61,11 +58,7 @@ class AliasDict(dict):
     def __delitem__(self, key):
         def single(d, target, value): del d[target]
 
-        def multi(d, target, value):
-            for subkey in self.aliases[key]:
-                del self[subkey]
-
         def unaliased(d, key, value):
             return super(AliasDict, d).__delitem__(key)
 
-        return self._handle(key, None, single, multi, unaliased)
+        return self._handle(key, None, single, None, unaliased)
