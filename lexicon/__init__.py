96d556c0f8cb7748b5e7a6fc29c87cf764233811
Jeff Forcier <jeff@bitprophet.org>
2012-05-22 15:11:50 -0700

    Add Lexicon merge class + tests

--- lexicon/__init__.py
+++ lexicon/__init__.py
@@ -3,3 +3,16 @@ from alias_dict import AliasDict
 
 
 __version__ = "0.1.0"
+
+
+class Lexicon(AttributeDict, AliasDict):
+    def __init__(self, *args, **kwargs):
+        # Need to avoid combining AliasDict's initial attribute write on
+        # self.aliases, with AttributeDict's __setattr__. Doing so results in
+        # an infinite loop. Instead, just skip straight to dict() for both
+        # explicitly (i.e. we override AliasDict.__init__ instead of extending
+        # it.)
+        # NOTE: could tickle AttributeDict.__init__ instead, in case it ever
+        # grows one.
+        dict.__init__(self, *args, **kwargs)
+        dict.__setattr__(self, 'aliases', {})
---
+++ tests/lexicon_.py
@@ -0,0 +1,29 @@
+from spec import Spec, eq_
+
+from lexicon import Lexicon
+
+
+class Lexicon_(Spec):
+    def attributes_work(self):
+        l = Lexicon()
+        l.foo = 'bar'
+        eq_(l['foo'], l.foo)
+
+    def aliases_work(self):
+        l = Lexicon()
+        l.alias('foo', to='bar')
+        l['bar'] = 'value'
+        assert l['foo'] == l['bar'] == 'value'
+
+    def aliases_appear_in_attributes(self):
+        l = Lexicon()
+        l.alias('foo', to='bar')
+        l.foo = 'value'
+        assert l.foo == l.bar == l['foo'] == l['bar'] == 'value'
+
+    def aliased_real_attributes_do_not_override_real_attributes(self):
+        l = Lexicon()
+        l.alias('get', to='notget')
+        l.notget = 'value'
+        assert callable(l.get)
+        assert l.get != 'value'
