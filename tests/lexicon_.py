26d53f476d4481374685fc2454a2a0bc81f5621f
Jeff Forcier <jeff@bitprophet.org>
2019-01-03 14:38:55 -0800

    Python 3.7 compat flake8

--- tests/lexicon_.py
+++ tests/lexicon_.py
@@ -7,36 +7,36 @@ from lexicon import Lexicon
 
 class Lexicon_(Spec):
     def attributes_work(self):
-        l = Lexicon()
-        l.foo = 'bar'
-        eq_(l['foo'], l.foo)
+        lex = Lexicon()
+        lex.foo = 'bar'
+        eq_(lex['foo'], lex.foo)
 
     def aliases_work(self):
-        l = Lexicon()
-        l.alias('foo', to='bar')
-        l['bar'] = 'value'
-        assert l['foo'] == l['bar'] == 'value'
+        lex = Lexicon()
+        lex.alias('foo', to='bar')
+        lex['bar'] = 'value'
+        assert lex['foo'] == lex['bar'] == 'value'
 
     def aliases_appear_in_attributes(self):
-        l = Lexicon()
-        l.alias('foo', to='bar')
-        l.foo = 'value'
-        assert l.foo == l.bar == l['foo'] == l['bar'] == 'value'
+        lex = Lexicon()
+        lex.alias('foo', to='bar')
+        lex.foo = 'value'
+        assert lex.foo == lex.bar == lex['foo'] == lex['bar'] == 'value'
 
     def aliased_real_attributes_do_not_override_real_attributes(self):
-        l = Lexicon()
-        l.alias('get', to='notget')
-        l.notget = 'value'
-        assert callable(l.get)
-        assert l.get != 'value'
+        lex = Lexicon()
+        lex.alias('get', to='notget')
+        lex.notget = 'value'
+        assert callable(lex.get)
+        assert lex.get != 'value'
 
     def ensure_deepcopy_works(self):
-        l = Lexicon()
-        l['foo'] = 'bar'
-        eq_(l.foo, 'bar')
-        l2 = copy.deepcopy(l)
-        l2.foo = 'biz'
-        assert l2.foo != l.foo
+        lex = Lexicon()
+        lex['foo'] = 'bar'
+        eq_(lex.foo, 'bar')
+        lex2 = copy.deepcopy(lex)
+        lex2.foo = 'biz'
+        assert lex2.foo != lex.foo
 
     def dir_only_shows_real_keys(self):
         "dir() only shows real keys-as-attrs, not aliases"
